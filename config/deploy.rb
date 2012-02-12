768db9a100e3aaafcb779199c3af91516177e0aa
Brandon Tilley <brandon@brandontilley.com>
2012-02-12 14:58:27 -0800

    Add setup_load_paths.rb for Passenger

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,22 +1,38 @@
-set :application, "set your application name here"
-set :repository,  "set your repository location here"
+$:.unshift(File.expand_path('./lib', ENV['rvm_path'])) # Add RVM's lib directory to the load path.
+require "rvm/capistrano"
+require "bundler/capistrano"
 
-set :scm, :subversion
-# Or: `accurev`, `bzr`, `cvs`, `darcs`, `git`, `mercurial`, `perforce`, `subversion` or `none`
+set :rvm_ruby_string, 'ruby-1.9.3'
+set :application, "battlemasters"
+set :repository,  "git@github.com:BinaryMuse/battlemasters-lfg.git"
+set :user,        "battlemasters"
+set :deploy_to,   "/var/www/vhosts/battlemasters.org/"
+set :scm,         :git
+set :use_sudo,    false
 
-role :web, "your web-server here"                          # Your HTTP server, Apache/etc
-role :app, "your app-server here"                          # This may be the same as your `Web` server
-role :db,  "your primary db-server here", :primary => true # This is where Rails migrations will run
-role :db,  "your slave db-server here"
+server "muse", :app, :web, :db, :primary => true
 
-# if you're still using the script/reaper helper you will need
-# these http://github.com/rails/irs_process_scripts
+namespace :deploy do
+  task :start do ; end
+  task :stop do ; end
+  task :restart, :roles => :app, :except => { :no_release => true } do
+    run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
+  end
+end
 
-# If you are using Passenger mod_rails uncomment this:
-# namespace :deploy do
-#   task :start do ; end
-#   task :stop do ; end
-#   task :restart, :roles => :app, :except => { :no_release => true } do
-#     run "#{try_sudo} touch #{File.join(current_path,'tmp','restart.txt')}"
-#   end
-# end
+namespace :db do
+  desc "Symlink the shared database.yml to the application config directory"
+  task :symlink do
+    run "ln -nfs #{shared_path}/config/database.yml #{release_path}/config/database.yml"
+  end
+end
+
+namespace :rvm do
+  desc 'Trust .rvmrc file'
+  task :trust_rvmrc do
+    run "rvm rvmrc trust #{current_release}"
+  end
+end
+
+after "deploy:update_code", "rvm:trust_rvmrc"
+after "deploy:finalize_update", "db:symlink"

fa20ec6fa93589700729d198587441ad88c8aa96
Brandon Tilley <brandon@brandontilley.com>
2012-02-11 19:44:31 -0800

    Initial commit: 'rails new battlemasters --skip-bundle --skip-test-unit'

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Battlemasters::Application.config.secret_token = '4d0880557c8559b8ff0ba0545041136dbf75d05b097282aeb9fc0442800114a708566c0d7a37ec4561d66b82257c4e9282377950bec9cf5c265abe447ef9c893'

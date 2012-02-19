1aaf1d615e2a4ce0eacfc3e1855143bc3b9549ef
Brandon Tilley <brandon@brandontilley.com>
2012-02-18 18:09:00 -0800

    Move Listing generator from rake task to db/seeds

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,14 +1,37 @@
-Listing.create!({
-  character: "Cyaga",
-  realm: "Nazjatar",
-  race: 6,
-  character_class: 11,
-  main_spec: "Restoration",
-  off_spec: "Feral Combat",
-  rating: 1234,
-  rank: "Peon",
-  resilience: 4000,
-  item_level: 500,
-  irc_name: "BinaryMuse",
-  notes: "Oh it's that guy again"
-})
+require 'faker'
+
+def realm_name
+  %w(
+    Aegwynn
+    Cho'gall
+    Dethecus
+    Drenden
+    Exodar
+    Garrosh
+    Grizzly Hills
+    Kel'Thuzad
+    Maiev
+    Ravenholdt
+  ).sample
+end
+
+Listing.delete_all
+10.times do |n|
+  klass = Wow::CLASSES.keys.sample
+  Listing.create!({
+    character: Faker::Name.first_name,
+    gender: rand(2),
+    realm: realm_name,
+    race: Wow::RACES.keys.sample,
+    character_class: klass,
+    main_spec: Wow::SPECS[klass].keys.sample,
+    off_spec: Wow::SPECS[klass].keys.sample,
+    rating: rand(2500),
+    rank: "Peon",
+    resilience: rand(5000),
+    item_level: (300..400).to_a.sample,
+    irc_name: Faker::Name.first_name,
+    notes: Faker::Lorem.sentence,
+    created_at: rand(n * 5).minutes.ago
+  })
+end

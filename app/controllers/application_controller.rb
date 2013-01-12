3d0f86a0d442916339a64983e6ca819e9b8148db
Brandon Tilley <brandon@brandontilley.com>
2013-01-12 13:12:56 -0800

    Temporarily closed for construction

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery
 
+  before_filter :redirect_to_uc
+  def redirect_to_uc
+    return render 'listings/uc'
+  end
+
   def present(object, klass = nil)
     klass ||= "#{object.class}Presenter".constantize
     klass.new object, view_context
